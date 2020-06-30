394e2e9502462d711136842a3d4191ed993e22d2
pzp <1459525376@qq.com>
2020-06-30 10:30:04 +0800

    完成页面3子页面静态部分+样式

---
+++ js/page3_2.js
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+
+//点击注册标签
+$(document).ready(function(){
+	$(".s3_2").click(function(){
+		console.log("xx1");
+
+		$(".s3_1")[0].style.background = "#c0c0c0";
+		$(".s3_2")[0].style.background = "#fafafa";
+		$(".s3_3")[0].style.background = "#c0c0c0";
+		$(".s3_4")[0].style.background = "#c0c0c0";
+		$(".s3_5")[0].style.background = "#c0c0c0";
+		$(".s3_6")[0].style.background = "#c0c0c0";
+		$("#page3_1")[0].style.display = "none";
+		$("#page3_2")[0].style.display = "block";
+		$("#page3_3")[0].style.display = "none";
+		$("#page3_4")[0].style.display = "none";
+		$("#page3_5")[0].style.display = "none";
+		$("#page3_6")[0].style.display = "none";
+	});
+});
