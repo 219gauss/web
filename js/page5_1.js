49878d1a2db4bf9d0f9ab3d6002944d9bd41797d
pzp <1459525376@qq.com>
2020-07-01 18:46:29 +0800

    完成页面5子页面静态部分+样式

---
+++ js/page5_1.js
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+
+//点击注册标签
+$(document).ready(function(){
+	$(".s5_1").click(function(){
+		console.log("xx1");
+
+		$(".s5_1")[0].style.background = "#fafafa";
+		$(".s5_2")[0].style.background = "#c0c0c0";
+		$(".s5_3")[0].style.background = "#c0c0c0";
+		$(".s5_4")[0].style.background = "#c0c0c0";
+		$(".s5_5")[0].style.background = "#c0c0c0";
+		$(".s5_6")[0].style.background = "#c0c0c0";
+		$(".s5_7")[0].style.background = "#c0c0c0";
+		$(".s5_8")[0].style.background = "#c0c0c0";
+		$(".s5_9")[0].style.background = "#c0c0c0";
+		$(".s5_10")[0].style.background = "#c0c0c0";
+		$(".s5_11")[0].style.background = "#c0c0c0";
+		$(".s5_12")[0].style.background = "#c0c0c0";
+		$(".s5_13")[0].style.background = "#c0c0c0";
+		$(".s5_14")[0].style.background = "#c0c0c0";
+		$(".s5_15")[0].style.background = "#c0c0c0";
+		$(".s5_16")[0].style.background = "#c0c0c0";
+		$("#page5_1")[0].style.display = "block";
+		$("#page5_2")[0].style.display = "none";
+		$("#page5_3")[0].style.display = "none";
+		$("#page5_4")[0].style.display = "none";
+		$("#page5_5")[0].style.display = "none";
+		$("#page5_6")[0].style.display = "none";
+		$("#page5_7")[0].style.display = "none";
+		$("#page5_8")[0].style.display = "none";
+		$("#page5_9")[0].style.display = "none";
+		$("#page5_10")[0].style.display = "none";
+		$("#page5_11")[0].style.display = "none";
+		$("#page5_12")[0].style.display = "none";
+		$("#page5_13")[0].style.display = "none";
+		$("#page5_14")[0].style.display = "none";
+		$("#page5_15")[0].style.display = "none";
+		$("#page5_16")[0].style.display = "none";
+	});
+});
